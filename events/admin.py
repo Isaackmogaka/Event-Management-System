2b922c919fd6aead5a96ac537a82328f86d459d7
Isaack Mogaka <isaacmogaka2016@gmail.com>
2025-03-18 19:58:35 +0300

    Initial commit - Set up Django project

---
+++ events/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Event  # Import your model
+
+admin.site.register(Event)  # Register the model
+
+# Register your models here.
---
+++ events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
